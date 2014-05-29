ff3fe83b71f8588260a4957a218de3b28d88d47a
EUREKIOS.com <info@eurekios.com>
2014-05-29 19:48:30 +0200

    Finish static pages

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -39,4 +39,7 @@ RSpec.configure do |config|
   # the seed, which is printed after each run.
   #     --seed 1234
   config.order = "random"
+
+  # agregar Capybara DSL para RSpec
+  config.include Capybara::DSL
 end
